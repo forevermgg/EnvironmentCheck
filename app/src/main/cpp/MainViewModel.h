50756e050e5ccf4982388d923f94bea7ef7ce60b
forevermgg <centforever@163.com>
2022-07-20 08:27:55 +0800

    commit message

--- app/src/main/cpp/MainViewModel.h
+++ app/src/main/cpp/MainViewModel.h
@@ -13,8 +13,8 @@ class MainViewModel : ViewModel {
   ~MainViewModel();
   virtual void unBind() override;
   virtual void bind() override;
-  virtual void handle(const std::string &key, const std::string &value);
-  virtual void handle(const int key, const std::string &value);
+  virtual void handle(const std::string &key, const std::string &value) override;
+  virtual void handle(const int key, const std::string &value) override;
 };
 
 #endif  // ENVIRONMENTCHECK_MAINVIEWMODEL_H
